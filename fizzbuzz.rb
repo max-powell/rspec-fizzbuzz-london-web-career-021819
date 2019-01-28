1aeeff0914d72187e2a668ae5e526dda33be7348
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-28 16:34:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
+  output = ''
+  if int % 3 == 0
+    output += 'Fizz'
+  end
+  if int % 5 == 0
+    output += 'Buzz'
+  if output == ''
+    return int
+  else
+    output
+  end
 end
