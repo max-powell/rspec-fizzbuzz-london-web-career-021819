8de870e65f8ae64b3500ec42d80e84430737d311
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-28 16:35:47 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  output = ''
+  if int % 3 == 0
+    output += 'Fizz'
+  end
+  if int % 5 == 0
+    output += 'Buzz'
+  end
+  if output != ''
+    output
+  end
+end
